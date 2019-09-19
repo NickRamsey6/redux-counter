b074e7e3f7f29bc98b07da60997f08aadaf2675c
Student <example@example.com>
2019-09-19 10:53:21 -0700

    add login functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {increment, decrement} from './actions';
+import {increment, decrement, login} from './actions';
 
 
 function App() {
@@ -9,7 +9,9 @@ function App() {
   const dispatch = useDispatch();
   return (
     <div className="App">
+      <button onClick={() => dispatch(login())}>Log in</button>
       <h1>Counter {counter}</h1>
+      <p>{isLogged}</p>
       <button onClick={()=> dispatch(increment(5))}>+</button>
       <button onClick={()=> dispatch(decrement())}>-</button>
 
